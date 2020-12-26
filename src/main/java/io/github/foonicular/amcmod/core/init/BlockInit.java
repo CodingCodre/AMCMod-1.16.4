829fcecf077296ee091fe45c61b550ad233effdd
Foonicular <julesisrad@icloud.com>
2020-12-26 10:29:48 -0800

    Added ruby

--- src/main/java/io/github/foonicular/amcmod/core/init/BlockInit.java
+++ src/main/java/io/github/foonicular/amcmod/core/init/BlockInit.java
@@ -1,5 +1,20 @@
 package io.github.foonicular.amcmod.core.init;
 
+import io.github.foonicular.amcmod.Main;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class BlockInit {
+
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
+
+	public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block",
+			() -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0f, 6.0f)
+					.sound(SoundType.METAL).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool()));
 
 }
--- src/main/java/io/github/foonicular/amcmod/core/init/ItemInit.java
+++ src/main/java/io/github/foonicular/amcmod/core/init/ItemInit.java
@@ -1,5 +1,16 @@
 package io.github.foonicular.amcmod.core.init;
 
+import io.github.foonicular.amcmod.Main;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ItemInit {
+
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
+
+	public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+			() -> new Item(new Item.Properties().group(Main.TAB)));
 
 }
